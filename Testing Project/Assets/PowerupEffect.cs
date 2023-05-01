81c1c24f5f301f70f41a8027088979a8caf2b6e4
DamienGarcia756811 <DamienLegend02@gmail.com>
2023-05-01 10:26:36 -0700

    update

---
+++ Testing Project/Assets/PowerupEffect.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Powerupeffect : ScriptableObject
+{
+
+    public abstract void Apply(GameObject target);
+}
+
---
+++ Testing Project/Assets/Script/PowerUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        if (collison.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
